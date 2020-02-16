117debe5f0cededf7cd5bf0d81b4ccb3c5875495
mingyu <mingyu@naver.com>
2020-02-16 15:28:26 +0900

    vue file

---
+++ front-end/API-vs-API/src/components/Vuetest.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Hello Vue</h2>
+
+</template>
+
+<script>
+
+    export default {
+      name: "Vuetest",
+        data:()=>{return{
+          testString:""
+        }},
+      methods:{
+        goTestPage(){
+          axios.get("http://localhost:8080/home.do")
+            .then(response => {
+              this.testString = response.data
+            })
+            .catch(e => {
+              console.log(e)
+            })
+        }
+      }
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
