9749ee66b80a8751f98ca44d2b883752715640ac
Seongjun2 <tjdwns4302@naver.com>
2020-02-16 13:52:41 +0900

    add initializer

---
+++ back-end/APIvsAPIBack/src/test/java/com/gigamachim/APIvsAPIBack/ApIvsApiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigamachim.APIvsAPIBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIvsApiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
