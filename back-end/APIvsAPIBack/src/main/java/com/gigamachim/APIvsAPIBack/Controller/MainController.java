35e6168acaae295fac0d0ae299c49db6607e649e
Seongjun2 <tjdwns4302@naver.com>
2020-02-28 13:01:48 +0900

    mybatis 테스트 중 에러

--- back-end/APIvsAPIBack/src/main/java/com/gigamachim/APIvsAPIBack/Controller/MainController.java
+++ back-end/APIvsAPIBack/src/main/java/com/gigamachim/APIvsAPIBack/Controller/MainController.java
@@ -1,10 +1,12 @@
 package com.gigamachim.APIvsAPIBack.Controller;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -16,5 +18,18 @@ public class MainController {
     @RequestMapping(value="test.do",method = RequestMethod.GET)
     public String printHello2(){
         return "We go Vue";
+    }
+
+    @Autowired
+    Test2Dao test2Dao;
+
+    @RequestMapping(value = "/mybatis.do", method = RequestMethod.GET)
+    public String home(Model md){
+        List<Test2Dto> list = test2Dao.sel();
+        for (int i = 0; i < list.size(); i++) {
+            md.addAttribute("NAME", list.get(0).getName());
+        }
+
+        return "home";
     }
 }
