64490db93742b285e29c53954c01895eb5287e9b
Seongjun2 <tjdwns4302@naver.com>
2020-03-05 12:26:49 +0900

    api 테스트 코드 추가

--- back-end/APIvsAPIBack/src/main/java/com/gigamachim/APIvsAPIBack/TestPack/APITest.java
+++ back-end/APIvsAPIBack/src/main/java/com/gigamachim/APIvsAPIBack/TestPack/APITest.java
@@ -26,14 +26,16 @@ package com.gigamachim.APIvsAPIBack.TestPack;
 
     */
 
-import java.io.File;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class APITest {
 
     StringBuffer reqStr = new StringBuffer();
     final String clientId = "tayx9y3qhl";
-    final String api_client_secret = "nDhUmCucfswlzH2AqiCsNrtKehSPpawTDBcJIbxh";
+    final String client_secret = "nDhUmCucfswlzH2AqiCsNrtKehSPpawTDBcJIbxh";
 
     public void request(){
 
@@ -42,8 +44,66 @@ public class APITest {
             String imgFile = "../../../../resources/";
             File uploadFile = new File(imgFile);
             String apiURL = "https://naveropenapi.apigw.ntruss.com/vision-obj/v1/detect"; // 객체 인식
+
             URL url = new URL(apiURL);
-            
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+
+            con.setUseCaches(false);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            //multipart request
+            String boundary = "---" + System.currentTimeMillis() + "---";
+            con.setRequestProperty("Content-type", "multipart/form-data; boundary" + boundary);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", client_secret);
+
+            OutputStream outputStream = con.getOutputStream();
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+            String LINE_FEED = "\r\n";
+
+            //file 추가
+            String fileName = uploadFile.getName();
+            writer.append("---" + boundary).append(LINE_FEED);
+            writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+            writer.append("Content-type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+            writer.append(LINE_FEED);
+            writer.flush();
+
+            FileInputStream inputStream = new FileInputStream(uploadFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while((bytesRead= inputStream.read(buffer)) != -1){
+                outputStream.write(buffer,0,bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+            writer.append(LINE_FEED).flush();
+            writer.append("--"+boundary+"--").append(LINE_FEED);
+            writer.close();
+
+            BufferedReader br = null;
+            int responseCode = con.getResponseCode();
+            if (responseCode == 200) {//정상
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            else{
+                System.out.println("error. responseCode = " + responseCode);
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            String inputLine;
+            if(br != null){
+                StringBuffer response = new StringBuffer();
+                while((inputLine = br.readLine()) != null){
+                    response.append(inputLine);
+                }
+
+                br.close();
+                System.out.println(response.toString());
+            }
+            else{
+                System.out.println("error");
+            }
         }
         catch(Exception e){
             e.printStackTrace();
