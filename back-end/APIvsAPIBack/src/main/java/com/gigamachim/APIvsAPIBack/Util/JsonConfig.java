51895751437bc77aedf9cdaece1ab01370d35079
Seongjun2 <tjdwns4302@naver.com>
2020-03-07 14:43:00 +0900

    add JsonConfig / json beauty and String to JsonObject

---
+++ back-end/APIvsAPIBack/src/main/java/com/gigamachim/APIvsAPIBack/Util/JsonConfig.java
@@ -0,0 +1,27 @@
+package com.gigamachim.APIvsAPIBack.Util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class JsonConfig {
+
+    public JsonObject StringToJson(String JsonStr) {
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = (JsonObject)jsonParser.parse(JsonStr);
+
+        return jsonObject;
+    }
+
+    public String jsonBeautify(String JsonStr){
+//        JsonParser parser = new JsonParser();
+//        JsonObject json = parser.parse(JsonStr).getAsJsonObject();
+        JsonObject jsonObject = StringToJson(JsonStr);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String prettyJson = gson.toJson(jsonObject);
+
+        return prettyJson;
+    }
+}
