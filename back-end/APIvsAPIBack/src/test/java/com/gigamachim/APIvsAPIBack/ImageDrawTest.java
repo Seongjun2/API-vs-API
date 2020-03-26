fc96b55055fc2ef327fb22f9820f4fcabf2f09ed
Seongjun2 <tjdwns4302@naver.com>
2020-03-26 15:59:13 +0900

    그리기 시도중

--- back-end/APIvsAPIBack/src/test/java/com/gigamachim/APIvsAPIBack/ImageDrawTest.java
+++ back-end/APIvsAPIBack/src/test/java/com/gigamachim/APIvsAPIBack/ImageDrawTest.java
@@ -1,10 +1,19 @@
 package com.gigamachim.APIvsAPIBack;
 
 import com.gigamachim.APIvsAPIBack.Util.ImageDraw;
+import com.gigamachim.APIvsAPIBack.Util.JsonConfig;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
 
 public class ImageDrawTest {
+    static int black = new Color(0,0,0).getRGB();
 
     public static void main(String[] args) {
 
@@ -17,5 +26,43 @@ public class ImageDrawTest {
         System.out.println("width = " + x + ", height = " + y);
 
 
+        String jsonString = "{\"predictions\": [{\"num_detections\": 1, \"detection_classes\": [1.0], \"detection_names\": [\"person\"], \"detection_scores\": [0.999369], \"detection_boxes\": [[0.141612, 0.283656, 1.0, 0.793158]]}]}";
+        JsonConfig jsonConfig = new JsonConfig();
+        JsonObject jsonObject = jsonConfig.StringToJson(jsonString);
+
+        JsonArray predictions = (JsonArray)jsonObject.get("predictions");
+
+        JsonObject elements = (JsonObject) predictions.get(0);
+        int numDetections = elements.get("num_detections").getAsInt();
+        JsonArray detectionClasses = (JsonArray) elements.get("detection_classes");
+        JsonArray detectionNames = (JsonArray)elements.get("detection_names");
+        JsonArray detectionScores = (JsonArray)elements.get("detection_scores");
+        JsonArray detectionBoxes = (JsonArray)elements.get("detection_boxes");
+
+        JsonArray firstList = (JsonArray)detectionBoxes.get(0);
+
+        for (int i = 0; i < firstList.size(); i++) {
+            System.out.println( (i+1) +" "+firstList.get(i));
+        }
+
+        for (int w = 0; w < x; w++) {
+            for (int h = 0; h < y; h++) {
+                image.setRGB(firstList.get(0).getAsInt()*320, firstList.get(1).getAsInt()*213, black);
+            }
+        }
+
+
+        try{
+            ImageIO.write(image, "jpg", new File("hello1.jpg"));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println();
+
+        /*
+
+        hello.jpg -> width : 320, height : 213
+         */
     }
 }
